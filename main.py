6d14fdb8b29d419740bbd81bfa852ef794f85531
Sady-Guzman <sady.guzman@userena.cl>
2025-01-08 14:40:25 -0300

    agrega feat. Eliminar tuplas, Cambia layout

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys
 from PySide6.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QPlainTextEdit, QPushButton
 from PySide6.QtWidgets import QApplication, QDialog, QHBoxLayout, QLineEdit, QLabel
-from ui_files.agrega_trapecios_c_layout import Ui_Dialog  # Import from the ui_files directory
+from PySide6.QtWidgets import QMessageBox, QApplication, QDialog
+from ui_files.agrega_trapecios_v2 import Ui_Dialog  # Import from the ui_files directory
+
 
 # QVLayout: layout_nuevas_row -> Se le agregan tuplas de valor de forma dinamica (manual y selec de catalogo)
 # historial_agregados -> Contador de tuplas que se agregaron dinamicamente a QVlayout
+        
 
 class MyDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Dialog()  # Create an instance of the UI class
-        self.ui.setupUi(self)  # Set up the UI on the dialog window
+    # def __init__(self):
+    #     super().__init__()
+    #     self.ui = Ui_Dialog()  # Create an instance of the UI class
+    #     self.ui.setupUi(self)  # Set up the UI on the dialog window
         
     def __init__(self):
         super().__init__()
@@ -19,14 +22,17 @@ class MyDialog(QDialog):
         self.dynamic_layouts = []  # Initialize dynamic layouts for row management
         self.historial_agregados = 0
 
-        # Connect the signal here
-        self.ui.btn_acpt_cant.clicked.connect(self.generate_layout)
+        # Conecta boton que acepta agregar tuplas a func generate_layout()
+        self.ui.btn_acpt_agregar.clicked.connect(self.generate_layout)
+
+        # Conecta boton que aceptar eliminar tuplas func del_rows()
+        self.ui.btn_acpt_eliminar.clicked.connect(self.del_rows)
 
 
     def generate_layout(self):
-        # Get the number of frames to generate from the spinbox
-        num_rows = self.ui.spin_cant.value()
-        print("SpinBox Cantidad a generar value: ", num_rows) # Debug
+        # obtiene el numero de tuplas a generar de la spinbox 'spin_cant_agregar'
+        num_rows = self.ui.spin_cant_agregar.value()
+        print("debug_print> SpinBox Cantidad a generar value: ", num_rows) # Debug
 
         # print("elementos dinamicamente agregados: ", self.historial_agregados) # Debug
         # Obtiene el numero de figuras(tuplas) ya existentes en el QVlayout contenedor, se usa para nombrar correctamente los nuevos elementos
@@ -51,6 +57,8 @@ class MyDialog(QDialog):
         
         ''' crea un nuevo Hlayout para agregar elementos de nueva tupla '''
         layout = QHBoxLayout()
+        # Asigna nombre a layout generado dinamicamente
+        layout_name = f"layout_t{index}"
         
 
         ''' Create the widgets for the row '''
@@ -97,10 +105,74 @@ class MyDialog(QDialog):
         self.historial_agregados += 1
 
 
+    ''' Elimina tuplas que fueron creadas dinamicamente dentro del layout vertical '''
+    def del_rows(self, index):
+        # obtiene el numero de tuplas a generar de la spinbox 'spin_cant_agregar'
+        num_rows = self.ui.spin_cant_eliminar.value()
+
+        # Asegura que la cantidad de layouts dinamicos que se van a intentar borrar no superen a la cantidad existente
+        # Si usa boton mientras spin=0, sale para evitar bug en contador dinamico
+        if num_rows > self.historial_agregados:
+            num_rows = self.historial_agregados
+        elif num_rows == 0:
+            return
+        
+        # Pide confirmacion de usuario antes de borrar layouts
+        reply = QMessageBox.question(self, 'Confirmar', 
+                                    "Confirmar: Eliminar cantidad de tuplas seleccionadas?", 
+                                    QMessageBox.Yes  | QMessageBox.No, 
+                                    QMessageBox.No)
+        if reply == QMessageBox.No:
+            return  # escapa con NO
+        
+        print("debug_print> SpinBox Cantidad a eliminar value: ", num_rows) # Debug
+
+        
+        ''' Elimina stretcher vertical en ultima posicion, se inserta nuevamente al final de proceso'''
+        if self.ui.layout_nuevas_row.itemAt(self.ui.layout_nuevas_row.count()-1).spacerItem():
+            # item = ultimo que se encuentra (vertical stretcher)
+            # Lo elimina mientras agrega nuevas tuplas, Lo vuelve a ingresar despues.
+            item = self.ui.layout_nuevas_row.takeAt(self.ui.layout_nuevas_row.count()-1)
+            del item
+
+
+        ''' Itera por los elementos de layout contenedor 'layout_nuevas_row' eliminando ultimos agregados'''
+        # Usa funcion 'delete_layout_widgets()
+        for _ in range(num_rows):
+            # Elimina la ultima tupla (layout) del layout contenedor
+            if self.ui.layout_nuevas_row.count() > 0:
+                last_item = self.ui.layout_nuevas_row.takeAt(self.ui.layout_nuevas_row.count() - 1)
+                
+                # comprueba que sea un layout (siempre deberia serlo) y usa funcion para eliminar iterando por sus elementos
+                if last_item.layout():
+                    self.delete_layout_widgets(last_item.layout())
+                del last_item  # elimina layout
+
+        ''' disminuye el contador de layouts que existen dinamicamente '''
+        self.historial_agregados -= num_rows
+
+        ''' se vuelve a insertar vertical stretcher en posicion inferior de layout vertical '''
+        self.ui.layout_nuevas_row.addStretch()
+
+    
+    
+    ''' Elimina los widgets dentro de un layout '''
+    def delete_layout_widgets(self, layout):
+        # Iterate through all the widgets in the layout and delete them
+        while layout.count():
+            item = layout.takeAt(0)  # Take the first item
+            if item.widget():  # If it's a widget, delete it
+                item.widget().deleteLater()
+            elif item.layout():  # If it's another layout, recursively delete its widgets
+                self.delete_layout_widgets(item.layout())  # Recursive call to delete nested layouts
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)  # Create the application
     dialog = MyDialog()            # Create the dialog window
     dialog.show()                  # Show the dialog window
     sys.exit(app.exec())           # Start the application's event loop
+
 
 
---
+++ ui_files/agrega_trapecios_v2.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'agrega_trapecios_v2.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QGridLayout,
+    QHBoxLayout, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QSpacerItem, QSpinBox, QVBoxLayout,
+    QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(1065, 776)
+        font = QFont()
+        font.setFamilies([u"Verdana"])
+        font.setBold(False)
+        font.setItalic(False)
+        Dialog.setFont(font)
+        self.verticalLayoutWidget = QWidget(Dialog)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(20, 10, 201, 125))
+        self.Vlayout_catalogo = QVBoxLayout(self.verticalLayoutWidget)
+        self.Vlayout_catalogo.setObjectName(u"Vlayout_catalogo")
+        self.Vlayout_catalogo.setContentsMargins(0, 0, 0, 0)
+        self.label_familia = QLabel(self.verticalLayoutWidget)
+        self.label_familia.setObjectName(u"label_familia")
+
+        self.Vlayout_catalogo.addWidget(self.label_familia)
+
+        self.combo_familia = QComboBox(self.verticalLayoutWidget)
+        self.combo_familia.addItem("")
+        self.combo_familia.addItem("")
+        self.combo_familia.addItem("")
+        self.combo_familia.setObjectName(u"combo_familia")
+
+        self.Vlayout_catalogo.addWidget(self.combo_familia)
+
+        self.label_modelo = QLabel(self.verticalLayoutWidget)
+        self.label_modelo.setObjectName(u"label_modelo")
+
+        self.Vlayout_catalogo.addWidget(self.label_modelo)
+
+        self.combo_modelo = QComboBox(self.verticalLayoutWidget)
+        self.combo_modelo.addItem("")
+        self.combo_modelo.addItem("")
+        self.combo_modelo.addItem("")
+        self.combo_modelo.setObjectName(u"combo_modelo")
+
+        self.Vlayout_catalogo.addWidget(self.combo_modelo)
+
+        self.horizontalLayoutWidget = QWidget(Dialog)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(20, 180, 751, 31))
+        self.Hlayout_label_variables = QHBoxLayout(self.horizontalLayoutWidget)
+        self.Hlayout_label_variables.setObjectName(u"Hlayout_label_variables")
+        self.Hlayout_label_variables.setContentsMargins(0, 0, 0, 0)
+        self.label_name = QLabel(self.horizontalLayoutWidget)
+        self.label_name.setObjectName(u"label_name")
+
+        self.Hlayout_label_variables.addWidget(self.label_name)
+
+        self.label_bi = QLabel(self.horizontalLayoutWidget)
+        self.label_bi.setObjectName(u"label_bi")
+
+        self.Hlayout_label_variables.addWidget(self.label_bi)
+
+        self.label_bs = QLabel(self.horizontalLayoutWidget)
+        self.label_bs.setObjectName(u"label_bs")
+
+        self.Hlayout_label_variables.addWidget(self.label_bs)
+
+        self.label_altura = QLabel(self.horizontalLayoutWidget)
+        self.label_altura.setObjectName(u"label_altura")
+
+        self.Hlayout_label_variables.addWidget(self.label_altura)
+
+        self.label_area = QLabel(self.horizontalLayoutWidget)
+        self.label_area.setObjectName(u"label_area")
+
+        self.Hlayout_label_variables.addWidget(self.label_area)
+
+        self.label_cg = QLabel(self.horizontalLayoutWidget)
+        self.label_cg.setObjectName(u"label_cg")
+
+        self.Hlayout_label_variables.addWidget(self.label_cg)
+
+        self.label_inercia = QLabel(self.horizontalLayoutWidget)
+        self.label_inercia.setObjectName(u"label_inercia")
+
+        self.Hlayout_label_variables.addWidget(self.label_inercia)
+
+        self.label_op = QLabel(self.horizontalLayoutWidget)
+        self.label_op.setObjectName(u"label_op")
+
+        self.Hlayout_label_variables.addWidget(self.label_op)
+
+        self.horizontalLayoutWidget_2 = QWidget(Dialog)
+        self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
+        self.horizontalLayoutWidget_2.setGeometry(QRect(20, 210, 751, 41))
+        self.Hlayout_tupla_base = QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.Hlayout_tupla_base.setObjectName(u"Hlayout_tupla_base")
+        self.Hlayout_tupla_base.setContentsMargins(0, 0, 0, 0)
+        self.t1_name = QLabel(self.horizontalLayoutWidget_2)
+        self.t1_name.setObjectName(u"t1_name")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_name)
+
+        self.t1_bi = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_bi.setObjectName(u"t1_bi")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_bi)
+
+        self.t1_bs = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_bs.setObjectName(u"t1_bs")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_bs)
+
+        self.t1_altura = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_altura.setObjectName(u"t1_altura")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_altura)
+
+        self.t1_area = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_area.setObjectName(u"t1_area")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_area)
+
+        self.t1_cg = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_cg.setObjectName(u"t1_cg")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_cg)
+
+        self.t1_inercia = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_inercia.setObjectName(u"t1_inercia")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_inercia)
+
+        self.t1_op = QLineEdit(self.horizontalLayoutWidget_2)
+        self.t1_op.setObjectName(u"t1_op")
+
+        self.Hlayout_tupla_base.addWidget(self.t1_op)
+
+        self.verticalLayoutWidget_3 = QWidget(Dialog)
+        self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
+        self.verticalLayoutWidget_3.setGeometry(QRect(800, 180, 121, 121))
+        self.Vlayout_label_resultados = QVBoxLayout(self.verticalLayoutWidget_3)
+        self.Vlayout_label_resultados.setObjectName(u"Vlayout_label_resultados")
+        self.Vlayout_label_resultados.setContentsMargins(0, 0, 0, 0)
+        self.label_sum_altura = QLabel(self.verticalLayoutWidget_3)
+        self.label_sum_altura.setObjectName(u"label_sum_altura")
+
+        self.Vlayout_label_resultados.addWidget(self.label_sum_altura)
+
+        self.label_sum_area = QLabel(self.verticalLayoutWidget_3)
+        self.label_sum_area.setObjectName(u"label_sum_area")
+
+        self.Vlayout_label_resultados.addWidget(self.label_sum_area)
+
+        self.label_sum_ponderado = QLabel(self.verticalLayoutWidget_3)
+        self.label_sum_ponderado.setObjectName(u"label_sum_ponderado")
+
+        self.Vlayout_label_resultados.addWidget(self.label_sum_ponderado)
+
+        self.label_sum_op = QLabel(self.verticalLayoutWidget_3)
+        self.label_sum_op.setObjectName(u"label_sum_op")
+
+        self.Vlayout_label_resultados.addWidget(self.label_sum_op)
+
+        self.verticalLayoutWidget_4 = QWidget(Dialog)
+        self.verticalLayoutWidget_4.setObjectName(u"verticalLayoutWidget_4")
+        self.verticalLayoutWidget_4.setGeometry(QRect(920, 180, 131, 116))
+        self.Vlayout_resultados = QVBoxLayout(self.verticalLayoutWidget_4)
+        self.Vlayout_resultados.setObjectName(u"Vlayout_resultados")
+        self.Vlayout_resultados.setContentsMargins(0, 0, 0, 0)
+        self.result_sum_altura = QLineEdit(self.verticalLayoutWidget_4)
+        self.result_sum_altura.setObjectName(u"result_sum_altura")
+        self.result_sum_altura.setReadOnly(True)
+
+        self.Vlayout_resultados.addWidget(self.result_sum_altura)
+
+        self.result_sum_area = QLineEdit(self.verticalLayoutWidget_4)
+        self.result_sum_area.setObjectName(u"result_sum_area")
+        self.result_sum_area.setReadOnly(True)
+
+        self.Vlayout_resultados.addWidget(self.result_sum_area)
+
+        self.result_sum_ponderado = QLineEdit(self.verticalLayoutWidget_4)
+        self.result_sum_ponderado.setObjectName(u"result_sum_ponderado")
+        self.result_sum_ponderado.setReadOnly(True)
+
+        self.Vlayout_resultados.addWidget(self.result_sum_ponderado)
+
+        self.result_sum_op = QLineEdit(self.verticalLayoutWidget_4)
+        self.result_sum_op.setObjectName(u"result_sum_op")
+        self.result_sum_op.setReadOnly(True)
+
+        self.Vlayout_resultados.addWidget(self.result_sum_op)
+
+        self.verticalLayoutWidget_5 = QWidget(Dialog)
+        self.verticalLayoutWidget_5.setObjectName(u"verticalLayoutWidget_5")
+        self.verticalLayoutWidget_5.setGeometry(QRect(20, 250, 751, 441))
+        self.layout_nuevas_row = QVBoxLayout(self.verticalLayoutWidget_5)
+        self.layout_nuevas_row.setObjectName(u"layout_nuevas_row")
+        self.layout_nuevas_row.setContentsMargins(0, 0, 0, 0)
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.layout_nuevas_row.addItem(self.verticalSpacer)
+
+        self.gridLayoutWidget = QWidget(Dialog)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(240, 10, 259, 91))
+        self.Glayout_edita_tuplas = QGridLayout(self.gridLayoutWidget)
+        self.Glayout_edita_tuplas.setObjectName(u"Glayout_edita_tuplas")
+        self.Glayout_edita_tuplas.setContentsMargins(0, 0, 0, 0)
+        self.label_cant_eliminar = QLabel(self.gridLayoutWidget)
+        self.label_cant_eliminar.setObjectName(u"label_cant_eliminar")
+
+        self.Glayout_edita_tuplas.addWidget(self.label_cant_eliminar, 0, 1, 1, 1)
+
+        self.spin_cant_agregar = QSpinBox(self.gridLayoutWidget)
+        self.spin_cant_agregar.setObjectName(u"spin_cant_agregar")
+
+        self.Glayout_edita_tuplas.addWidget(self.spin_cant_agregar, 1, 0, 1, 1)
+
+        self.btn_acpt_eliminar = QPushButton(self.gridLayoutWidget)
+        self.btn_acpt_eliminar.setObjectName(u"btn_acpt_eliminar")
+
+        self.Glayout_edita_tuplas.addWidget(self.btn_acpt_eliminar, 2, 1, 1, 1)
+
+        self.btn_acpt_agregar = QPushButton(self.gridLayoutWidget)
+        self.btn_acpt_agregar.setObjectName(u"btn_acpt_agregar")
+
+        self.Glayout_edita_tuplas.addWidget(self.btn_acpt_agregar, 2, 0, 1, 1)
+
+        self.spin_cant_eliminar = QSpinBox(self.gridLayoutWidget)
+        self.spin_cant_eliminar.setObjectName(u"spin_cant_eliminar")
+
+        self.Glayout_edita_tuplas.addWidget(self.spin_cant_eliminar, 1, 1, 1, 1)
+
+        self.label_cant_agregar = QLabel(self.gridLayoutWidget)
+        self.label_cant_agregar.setObjectName(u"label_cant_agregar")
+
+        self.Glayout_edita_tuplas.addWidget(self.label_cant_agregar, 0, 0, 1, 1)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.label_familia.setText(QCoreApplication.translate("Dialog", u"Familia", None))
+        self.combo_familia.setItemText(0, QCoreApplication.translate("Dialog", u"Elegir", None))
+        self.combo_familia.setItemText(1, QCoreApplication.translate("Dialog", u"VI", None))
+        self.combo_familia.setItemText(2, QCoreApplication.translate("Dialog", u"VF", None))
+
+        self.label_modelo.setText(QCoreApplication.translate("Dialog", u"Modelo", None))
+        self.combo_modelo.setItemText(0, QCoreApplication.translate("Dialog", u"Elegir", None))
+        self.combo_modelo.setItemText(1, QCoreApplication.translate("Dialog", u"4120", None))
+        self.combo_modelo.setItemText(2, QCoreApplication.translate("Dialog", u"9kjahsd", None))
+
+        self.label_name.setText(QCoreApplication.translate("Dialog", u"Num", None))
+        self.label_bi.setText(QCoreApplication.translate("Dialog", u"Base_inf", None))
+        self.label_bs.setText(QCoreApplication.translate("Dialog", u"Base_sup", None))
+        self.label_altura.setText(QCoreApplication.translate("Dialog", u"Altura", None))
+        self.label_area.setText(QCoreApplication.translate("Dialog", u"Area", None))
+        self.label_cg.setText(QCoreApplication.translate("Dialog", u"Cg_Sup", None))
+        self.label_inercia.setText(QCoreApplication.translate("Dialog", u"I(nercia?)", None))
+        self.label_op.setText(QCoreApplication.translate("Dialog", u"I+A*r^2", None))
+        self.t1_name.setText(QCoreApplication.translate("Dialog", u"T1\t    ", None)) # usa tab + 4 espacios( 1/2 tab) para coincidir, Aumenta el numero mostrado iterativamente
+        self.label_sum_altura.setText(QCoreApplication.translate("Dialog", u"Sum Altura", None))
+        self.label_sum_area.setText(QCoreApplication.translate("Dialog", u"Sum Area", None))
+        self.label_sum_ponderado.setText(QCoreApplication.translate("Dialog", u"Sum Ponderado", None))
+        self.label_sum_op.setText(QCoreApplication.translate("Dialog", u"Sum I+A*r^2", None))
+        self.label_cant_eliminar.setText(QCoreApplication.translate("Dialog", u"Eliminar Secciones", None))
+        self.btn_acpt_eliminar.setText(QCoreApplication.translate("Dialog", u"Aceptar", None))
+        self.btn_acpt_agregar.setText(QCoreApplication.translate("Dialog", u"Aceptar", None))
+        self.label_cant_agregar.setText(QCoreApplication.translate("Dialog", u"Agregar Secciones", None))
+    # retranslateUi
+
