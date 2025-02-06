30fdb9b951d364aeb188dd77585d8946988803f1
Sady-Guzman <sady.guzman@userena.cl>
2025-02-06 16:44:50 -0300

    anade gradiente controlado para grafico viga

--- dibujo.py
+++ dibujo.py
@@ -7,8 +7,8 @@ import os
 DB_DIR = os.path.join(os.path.dirname(__file__), "databases")
 
 # Genera colores en gradientes de verde
-def color_personalizado():
-    green_intensity = random.uniform(0.6, 0.9)  # varia entre verde oscuro a verde claro
+def color_personalizado(tono_verde):
+    green_intensity = tono_verde  # varia entre verde oscuro a verde claro
     red_intensity = green_intensity * random.uniform(0.0, 0.15)  
     blue_intensity = green_intensity * random.uniform(0.0, 0.15) 
     return [red_intensity, green_intensity, blue_intensity]
@@ -44,8 +44,10 @@ def plot_trapecios(pieza_id, seccion, familia, modelo, es_creada):
     y_offset = 0  # Offset para alinear los trapecios verticalmente (acumula altura de cada trap.)
     max_base_inf = max(base_inf for base_inf, _, _ in trapecios)  # encuentra el trapecio con la base mas grande para centrar fig
 
+    tono_color = 0.55
     # Itera todos lso trapecios para 
     for base_inf, base_sup, altura in trapecios:
+
         # Calculo el offset horizontal hasta el centro del trapecio
         horizontal_offset = (max_base_inf - base_inf) / 2
 
@@ -58,9 +60,13 @@ def plot_trapecios(pieza_id, seccion, familia, modelo, es_creada):
             (horizontal_offset + (base_inf - base_sup) / 2, y_offset + altura)  # Top-left
         ]
 
+        # Asegura que no se pase de 0.99 en alpha de color verde
+        if tono_color > 0.99:
+            tono_color = 0.7
 
         # Usa funcion de color todo de verde
-        color = color_personalizado()
+        color = color_personalizado(tono_color)
+        tono_color += 0.04
 
 
         # Agrega el trapecio al plot
