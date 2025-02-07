57db3c85b2e2054346992a88d44a4dd463d0da2c
Sady-Guzman <sady.guzman@userena.cl>
2025-02-07 09:42:31 -0300

    implementa del barra a.Act. / Add barra al final en vez de inicio layout

--- armadura_pasiva.py
+++ armadura_pasiva.py
@@ -12,6 +12,7 @@ def setup_armadura_pasiva(self):
     armpas_llenar_combos_materiales(self)
 
     self.ui.tab3_btn_add_barras.clicked.connect(lambda: apasiva_add_barra_corrugada(self))
+    self.ui.tab3_btn_del_barras.clicked.connect(lambda: apasiva_del_barra_corrugada(self))
 
 
 
@@ -35,68 +36,6 @@ def armpas_llenar_combos_materiales(self):
         self.ui.tab3_combo_tipo_mallas.addItem(f"{materiales_arm_pasiva[i][0]}")
 
 
-''' ====== '''
-''' STRCUTURE WANTED FOR EACH ROW (BARRA CORRUGADA '''
-''' tab3_horizontal_layout would be the name of the horizontal layout being added to the vertical layout. It doesnt need that name, Because its reference will be stored in a variable'''
-''' tab3_Vlayout_barras is the VerticalLayout that will contain all the other HorizontalLayouts for each Barra Corrugada. The Equivalent in the example code is self.ui.layout_nuevas_row '''
-
-
-
-''' 
-self.tab3_horizontal_layout = QHBoxLayout()
-self.tab3_horizontal_layout.setObjectName(u"tab3_horizontal_layout")
-    
-    self.tab3_line_pos1 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos1.setObjectName(u"tab3_line_pos1")
-    self.tab3_line_pos1.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos1)
-
-    self.tab3_combo_pos2 = QComboBox(self.verticalLayoutWidget_2)
-    self.tab3_combo_pos2.addItem("")
-    self.tab3_combo_pos2.setObjectName(u"tab3_combo_pos2")
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_combo_pos2)
-
-    self.tab3_line_pos3 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos3.setObjectName(u"tab3_line_pos3")
-    self.tab3_line_pos3.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos3)
-
-    self.tab3_line_pos4 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos4.setObjectName(u"tab3_line_pos4")
-    self.tab3_line_pos4.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos4)
-
-    self.tab3_line_pos5 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos5.setObjectName(u"tab3_line_pos5")
-    self.tab3_line_pos5.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos5)
-
-    self.tab3_line_pos6 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos6.setObjectName(u"tab3_line_pos6")
-    self.tab3_line_pos6.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos6)
-
-    self.tab3_line_pos7 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos7.setObjectName(u"tab3_line_pos7")
-    self.tab3_line_pos7.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos7)
-
-    self.tab3_line_pos8 = QLineEdit(self.verticalLayoutWidget_2)
-    self.tab3_line_pos8.setObjectName(u"tab3_line_pos8")
-    self.tab3_line_pos8.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos8)
-
-self.tab3_Vlayout_barras.addLayout(self.tab3_horizontal_layout)
-'''
-
 
 
 def apasiva_add_barra_corrugada(self):
@@ -117,8 +56,8 @@ def apasiva_add_barra_corrugada(self):
     ''' Crea los widgets para la fila '''
 
     line_pos1 = QLineEdit()
-    line_pos2 = QLineEdit()
-    combo_pos3 = QComboBox()
+    combo_pos2 = QComboBox()
+    line_pos3 = QLineEdit()
     line_pos4 = QLineEdit()
     line_pos5 = QLineEdit()
     line_pos6 = QLineEdit()
@@ -127,8 +66,8 @@ def apasiva_add_barra_corrugada(self):
 
     ''' ajusta centrado '''
     line_pos1.setAlignment(Qt.AlignmentFlag.AlignCenter)
-    line_pos2.setAlignment(Qt.AlignmentFlag.AlignCenter)
-    # combo_pos3.setAlignment(Qt.AlignmentFlag.AlignCenter) # No aplica centrado de texto en ComboBox
+    # combo_pos2.setAlignment(Qt.AlignmentFlag.AlignCenter) # No aplica centrado de texto en ComboBox
+    line_pos3.setAlignment(Qt.AlignmentFlag.AlignCenter)
     line_pos4.setAlignment(Qt.AlignmentFlag.AlignCenter)
     line_pos5.setAlignment(Qt.AlignmentFlag.AlignCenter)
     line_pos6.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -137,22 +76,71 @@ def apasiva_add_barra_corrugada(self):
 
     ''' Agrega componentes a layout horizontal '''
     layout.addWidget(line_pos1)
-    layout.addWidget(line_pos2)
-    layout.addWidget(combo_pos3)
+    layout.addWidget(combo_pos2)
+    layout.addWidget(line_pos3)
     layout.addWidget(line_pos4)
     layout.addWidget(line_pos5)
     layout.addWidget(line_pos6)
     layout.addWidget(line_pos7)
     layout.addWidget(line_pos8)
+
+    ''' Anade al final de diccionario la nueva barra '''
+    self.dynamic_apasiva_barras.append({
+        "posicion": line_pos1,
+        "uso": combo_pos2,
+        "n_min": line_pos3,
+        "n_max": line_pos4,
+        "diametro_min": line_pos5,
+        "diametro_max": line_pos6,
+        "cota": line_pos7,
+        "longitud": line_pos8,
+    })
     
 
     ''' Inserta horizontal layout recien generado a Vertical layout tab3_Vlayout_barras '''
-    self.ui.tab3_Vlayout_barras.insertLayout(0, layout)
+    # self.ui.tab3_Vlayout_barras.insertLayout(0, layout)
+    self.ui.tab3_Vlayout_barras.addLayout(layout)
 
     ''' Vuelve a insertar el vertical stretcher en la posición inferior del layout vertical '''
     self.ui.tab3_Vlayout_barras.addStretch()
 
+def apasiva_del_barra_corrugada(self):
+    ''' asd '''
+    
+    ''' Elimina el vertical stretcher temporalmente '''
+    if self.ui.tab3_Vlayout_barras.itemAt(self.ui.tab3_Vlayout_barras.count() - 1).spacerItem():
+        item = self.ui.tab3_Vlayout_barras.takeAt(self.ui.tab3_Vlayout_barras.count() - 1)
+        del item  # Remove the stretcher
+    
+    if self.ui.tab3_Vlayout_barras.count() > 0:
+        # Posicion de ultimo item en layout veretical
+        last_index = self.ui.tab3_Vlayout_barras.count() - 1
 
+        # Toma el último elemento (último layout añadido)
+        last_item = self.ui.tab3_Vlayout_barras.takeAt(last_index) # Should be last element !
+
+        # Comprueba que el item sea un layout antes de eliminarlo
+        if last_item.layout():
+            delete_layout_widgets(self, last_item.layout())  # Elimina los widgets dentro del layout primero
+        del last_item  # Elimina el layout
+
+        # Elimina referencia al layout dinámico (último visualmente)
+        # self.dynamic_layouts.pop(0)  # Elimina el primer elemento de la lista de layouts dinámicos
+        self.dynamic_apasiva_barras.pop()
+
+        # Vuelve a agregar Vertical spacer
+        self.ui.tab3_Vlayout_barras.addStretch()
+
+
+''' Elimina los widgets dentro de un layout '''
+def delete_layout_widgets(self, layout):
+    # Iterate through all the widgets in the layout and delete them
+    while layout.count():
+        item = layout.takeAt(0)  # Take the first item
+        if item.widget():  # If it's a widget, delete it
+            item.widget().deleteLater()
+        elif item.layout():  # If it's another layout, recursively delete its widgets
+            self.delete_layout_widgets(item.layout())  # Recursive call to delete nested layouts
 
 
 ''' ================================================================================================================================================ '''
@@ -162,6 +150,63 @@ def apasiva_add_barra_corrugada(self):
 ''' ================================================================================================================================================ '''
 ''' ================================================================================================================================================ '''
 ''' ================================================================================================================================================ '''
+
+
+# ''' Elimina trapecio seleccionado en ListWidget '''
+# def del_rows(self):
+#     """ Deletes the corresponding row from VLayout based on the label text. """
+    
+#     try:
+#         trapecio_para_borrar = self.ui.tab1_list_trapecios_existentes.currentItem().text()
+#     except:
+#         print("Error: No hay ningun trapecio seleccionado para eliminar.")
+#         return
+#     # Find the matching row dynamically
+#     row_to_delete = None
+#     for row in self.dynamic_layouts:
+
+#         if row["name_label"].text().strip() == trapecio_para_borrar:
+#             row_to_delete = row
+#             break
+
+#     if not row_to_delete:
+#         print(f"Error: Could not find row with label {trapecio_para_borrar}")
+#         return
+
+#     # Remove the row's layout from the VLayout
+#     for i in range(self.ui.layout_nuevas_row.count()):
+#         item = self.ui.layout_nuevas_row.itemAt(i)
+#         if isinstance(item, QHBoxLayout) and row_to_delete["name_label"] in [item.itemAt(j).widget() for j in range(item.count())]:
+#             layout_to_remove = self.ui.layout_nuevas_row.takeAt(i)
+#             # Delete all widgets inside the layout
+#             while layout_to_remove.count():
+#                 item = layout_to_remove.takeAt(0)
+#                 widget = item.widget()
+#                 if widget:
+#                     widget.deleteLater()  # Ensure Qt properly removes it
+#             del layout_to_remove
+#             break
+
+#     # Remove from tracking list
+#     self.dynamic_layouts.remove(row_to_delete)
+#     self.historial_agregados -= 1
+#     self.ui.tab1_list_trapecios_existentes.takeItem(int(trapecio_para_borrar[1])-1)
+
+#     self.ui.tab1_list_trapecios_existentes.clear()
+#     for index, row in enumerate(reversed(self.dynamic_layouts)):
+#         # Ajusta name_label de todos los trapecios
+#         row["name_label"].setText(f"T{index+1}\t    ")
+
+#         # Agrega name_label de trapecio a LIST para luego poder seleccionalo y borrarlo
+#         self.ui.tab1_list_trapecios_existentes.addItem(f"T{index+1}")
+
+#     print(f"Se elimina trapecio correctamente")
+
+
+
+
+
+
 # ''' genera nuevo Hlayout (dinamico) y sus elementos, Los nombra correctamente y agrega a Vlayout contenedor (layout fijo)'''
 # def add_rows(self, index):
 #     ''' Maneja vertical stretcher para solo tener 1 y que siempre esté abajo '''
@@ -233,55 +278,67 @@ def apasiva_add_barra_corrugada(self):
 
 
 
-# ''' Elimina trapecio seleccionado en ListWidget '''
-# def del_rows(self):
-#     """ Deletes the corresponding row from VLayout based on the label text. """
+
+
+
+''' ====== '''
+''' STRCUTURE WANTED FOR EACH ROW (BARRA CORRUGADA '''
+''' tab3_horizontal_layout would be the name of the horizontal layout being added to the vertical layout. It doesnt need that name, Because its reference will be stored in a variable'''
+''' tab3_Vlayout_barras is the VerticalLayout that will contain all the other HorizontalLayouts for each Barra Corrugada. The Equivalent in the example code is self.ui.layout_nuevas_row '''
+
+
+
+''' 
+self.tab3_horizontal_layout = QHBoxLayout()
+self.tab3_horizontal_layout.setObjectName(u"tab3_horizontal_layout")
     
-#     try:
-#         trapecio_para_borrar = self.ui.tab1_list_trapecios_existentes.currentItem().text()
-#     except:
-#         print("Error: No hay ningun trapecio seleccionado para eliminar.")
-#         return
-#     # Find the matching row dynamically
-#     row_to_delete = None
-#     for row in self.dynamic_layouts:
+    self.tab3_line_pos1 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos1.setObjectName(u"tab3_line_pos1")
+    self.tab3_line_pos1.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-#         if row["name_label"].text().strip() == trapecio_para_borrar:
-#             row_to_delete = row
-#             break
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos1)
 
-#     if not row_to_delete:
-#         print(f"Error: Could not find row with label {trapecio_para_borrar}")
-#         return
+    self.tab3_combo_pos2 = QComboBox(self.verticalLayoutWidget_2)
+    self.tab3_combo_pos2.addItem("")
+    self.tab3_combo_pos2.setObjectName(u"tab3_combo_pos2")
 
-#     # Remove the row's layout from the VLayout
-#     for i in range(self.ui.layout_nuevas_row.count()):
-#         item = self.ui.layout_nuevas_row.itemAt(i)
-#         if isinstance(item, QHBoxLayout) and row_to_delete["name_label"] in [item.itemAt(j).widget() for j in range(item.count())]:
-#             layout_to_remove = self.ui.layout_nuevas_row.takeAt(i)
-#             # Delete all widgets inside the layout
-#             while layout_to_remove.count():
-#                 item = layout_to_remove.takeAt(0)
-#                 widget = item.widget()
-#                 if widget:
-#                     widget.deleteLater()  # Ensure Qt properly removes it
-#             del layout_to_remove
-#             break
+    self.tab3_horizontal_layout.addWidget(self.tab3_combo_pos2)
 
-#     # Remove from tracking list
-#     self.dynamic_layouts.remove(row_to_delete)
-#     self.historial_agregados -= 1
-#     self.ui.tab1_list_trapecios_existentes.takeItem(int(trapecio_para_borrar[1])-1)
+    self.tab3_line_pos3 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos3.setObjectName(u"tab3_line_pos3")
+    self.tab3_line_pos3.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos3)
 
-#     self.ui.tab1_list_trapecios_existentes.clear()
-#     for index, row in enumerate(reversed(self.dynamic_layouts)):
-#         # Ajusta name_label de todos los trapecios
-#         row["name_label"].setText(f"T{index+1}\t    ")
+    self.tab3_line_pos4 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos4.setObjectName(u"tab3_line_pos4")
+    self.tab3_line_pos4.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-#         # Agrega name_label de trapecio a LIST para luego poder seleccionalo y borrarlo
-#         self.ui.tab1_list_trapecios_existentes.addItem(f"T{index+1}")
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos4)
 
+    self.tab3_line_pos5 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos5.setObjectName(u"tab3_line_pos5")
+    self.tab3_line_pos5.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos5)
 
-#     print(f"Se elimina trapecio correctamente")
+    self.tab3_line_pos6 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos6.setObjectName(u"tab3_line_pos6")
+    self.tab3_line_pos6.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos6)
+
+    self.tab3_line_pos7 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos7.setObjectName(u"tab3_line_pos7")
+    self.tab3_line_pos7.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos7)
+
+    self.tab3_line_pos8 = QLineEdit(self.verticalLayoutWidget_2)
+    self.tab3_line_pos8.setObjectName(u"tab3_line_pos8")
+    self.tab3_line_pos8.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    self.tab3_horizontal_layout.addWidget(self.tab3_line_pos8)
+
+self.tab3_Vlayout_barras.addLayout(self.tab3_horizontal_layout)
+'''
